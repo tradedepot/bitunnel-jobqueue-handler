4e51ced6a0c136c3b27f86c89fdabfa633bae792
Onyemaechi Okafor <ookafor@c2gconsulting.com>
2016-08-13 02:00:13 +0100

    Refactor and optimize index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ let middlewareEventUrl = process.env.MIDDLEWARE_EVENT_URL || 'https://sandbox.tr
 const makeNtlmRequest = lastNo => {
   return new Promise((res, rej) => {
     let url = process.env.ODATA_JOBQ_URL || "http://52.4.6.204:4068/PROMASIDOR_NAV/OData/Company('PROMASIDOR%20Nigeria')/tdmiddlewarevent?$format=json";
-    let nextNumber = parseInt(lastNo) + 100;
+    let nextNumber = parseInt(lastNo) + parseInt((process.env.BATCH_SIZE ||"1000"));
 
-    // nextNumber = utils.pad(nextNumber, 8);
+    nextNumber = utils.pad(nextNumber, 8);
 
-    url += `&$filter=No gt '${lastNo}'`;
-    // url += `&$filter=No gt '${lastNo}' and No lt '${nextNumber}'`;
+    //url += `&$filter=No gt '${lastNo}'`;
+    url += `&$filter=No gt '${lastNo}' and No lt '${nextNumber}'`;
 
     console.log(url)
 
@@ -35,15 +35,30 @@ const makeNtlmRequest = lastNo => {
 
 //make http request
 const sendToBitunnel = (opt) => {
-  return new Promise((res, rej) => {
-    request(opt, (error, response, body) => {
-      if (!error) {
-        res(body);
-      } else {
-        rej(error);
-      }
-    })
-  })
+  return (seqNo,results,i)=> {
+    return new Promise((res, rej) => {
+      //testing...
+      /**if(i==200){
+        setTimeout(() => {
+          let error="Some random error";
+          results[i]={error: error,seqNo: seqNo, index: i}
+          console.log("error",results[i]);
+          rej(error);
+        }, i*10*5);
+      }else{**/
+        setTimeout(() => {
+          request(opt, (error, response, body) => {
+                results[i]={error: error,seqNo: seqNo}
+                if (!error) {
+                  res(body);
+                } else {
+                  rej(error);
+                }
+            });
+          }, i*10);
+        //}
+      });
+    }
 }
 
 const getMdPayload = (object, headers, url) => {
@@ -71,46 +86,86 @@ const onRun = () => {
       if (resp) {
         resp = JSON.parse(resp);
         let events = resp.value;
-        if (_.size(events) > 0) {
-          let promises = [],
-            nos = _.pluck(events, 'No');
-
-          _.each(events, (event, i) => {
-            setTimeout(() => {
-              let _event = {
-                "id": "string",
-                "callbackUrl": "string",
-                "createTime": 0,
-                "resourceID": event.EventKey,
-                "eventType": event.EventType
-              };
-
-              let promise = sendToBitunnel(getMdPayload(_event, { tenant_id: process.env.TENANT_ID || "PROMASIDOR_TEST", origin_user: event.OriginUser }, middlewareEventUrl))
-              promises.push(promise);
-            }, 10 * i);
-
-          })
-
-          Promise.all(promises)
-            .then((success) => {
-
-              nos = _.uniq(nos);
+        //console.log("events",events);
+        let n=events.length;
+        if (n > 0) {
+            const results=new Array(n);
+            /*
+              Events are externally sorted by the no, no need to sort because it takes O(NlogN) time to sort.
+              NAV has done it for us in d query because No is primary key which is default sort key.
+              we can use this in more generic cases where we are nt sure of the sort order.
+              nos = _.pluck(events, 'No')
+              nos = _.uniq(nos);//
               let sorted = _.sortBy(nos, no => no);
-              let last = _.last(sorted);
+            **/
+
+            let promises=_.map(events, (event, i) => {
+                let _event = {
+                  "id": event.No,
+                  "callbackUrl": "string",
+                  "createTime": 0,
+                  "resourceID": event.EventKey,
+                  "eventType": event.EventType
+                };
+                return sendToBitunnel(getMdPayload(_event, { tenant_id: process.env.TENANT_ID || "PROMASIDOR_TEST", origin_user: event.OriginUser }, middlewareEventUrl))(event.No,results,i);
+            });
+            Promise.all(promises)
+            .then((success) => {
+              let last = events[n-1].No;
               if (last) {
                 redisUtil.setLastFetchedNo(last);
+              }else{
+                logBitunnelError(generateError(last,events[n-1]));
               }
-
             })
             .catch(error => {
-              console.log(`An error occured while sending events to middleware, ${error}`);
+              //get the last successful contiguous seqNo greedily. this is the seqNo of the event before the first error if it exist
+              let lastSuccess=Number.MIN_VALUE; //-2^31
+              let firstErrorIndx=-1;
+              for(let i=0;i<results.length;i++){
+                if(typeof results[i] === 'undefined' || results[i].error){
+                    firstErrorIndx=i;
+                    break;
+                }
+              }
+              if(firstErrorIndx-1>=0){
+                lastSuccess=results[firstErrorIndx-1].seqNo;
+              }
+              redisUtil.getLastFetchedNo()
+                .then(lastNo => {
+                    if(lastSuccess!=Number.MIN_VALUE){
+                      if(parseInt(lastSuccess)>parseInt(lastNo)){
+                          lastNo=lastSuccess;
+                          redisUtil.setLastFetchedNo(lastNo);
+                      }
+                      logBitunnelError(generateError(lastNo,error));
+                  }else{
+                      logBitunnelError(generateError(lastNo,error));
+                  }
+                });
+
+                logBitunnelError(generateError(lastNo,error));
             });
         }
       }
     })
     .catch(error => {
-      console.log(`Error: ${error}`);
+      logBitunnelError(`${error}`);
     })
+}
+
+const generateError = (seqNo,error) => {
+    return {error: error,lastSeqNo: seqNo};
+}
+
+const logBitunnelError = (error) => {
+    error={
+      errorMessage:`BitunnelJobQueueError: ${JSON.stringify(error)}`,
+      body: error,
+      source: "BitunnelJobQueueHandler"
+    }
+    //send ds over the wire using ...
+   console.log(error);
 }
 
 const startJob = cronPattern => {
@@ -121,18 +176,19 @@ const startJob = cronPattern => {
       start: true
     });
   } catch (ex) {
-    console.log(`error in job ${ex}`);
+    logBitunnelError(`error in job ${ex}`);
   }
 }
 
 //get cron Pattern or run every 1 minutes
+//Is possible to set cron to run 1 minute after a run completes?
 redisUtil.getCronPattern()
   .then((cronPattern) => {
     // startJob(cronPattern || '0 */1  * * * *');
-    startJob(cronPattern || '*/10 *  * * * *'); //10secs
+    startJob(cronPattern || '*/30 *  * * * *'); //10secs
   })
   .catch((exp) => {
-    console.log(`Error pattern: ${exp}`);
+    logBitunnelError(`Error pattern: ${exp}`);
   })
 
 
