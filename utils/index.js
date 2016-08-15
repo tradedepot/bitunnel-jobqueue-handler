14d88c9527570e84ea7f676af231cb25abaa203c
Kingsley Ochu <kochu@c2gconsulting.com>
2016-08-15 10:17:50 +0100

    Annotate middleware dispatch

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,38 @@
 'use strict';
+ const _ = require('underscore');
 
 exports.pad = (num, size) => {
   let s = num + "";
   while (s.length < size) s = "0" + s;
   return s;
+}
+
+exports.logBitunnelError = error => {
+  error = {
+      errorMessage: `BitunnelJobQueueError: ${JSON.stringify(error)}`,
+      body: error,
+      source: "BitunnelJobQueueHandler"
+    }
+    //send ds over the wire using ...
+  console.error(error);
+}
+
+exports.generateError = (seqNo, error) => {
+  return { error: error, lastSeqNo: seqNo };
+}
+
+exports.constructMdPayload = (object, headers, url) => {
+  headers = _.extend({
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }, headers);
+
+  return {
+    url: url,
+    method: 'POST',
+    headers: headers,
+    body: object,
+    json: true,
+    strictSSL: false,
+  }
 }
