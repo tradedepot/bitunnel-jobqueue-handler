5686ffae756eab493661c8fd7a73dfc8e955a72d
Kingsley Ochu <kochu@c2gconsulting.com>
2016-10-04 10:05:21 +0100

    Update template

--- utils/mail-template.js
+++ utils/mail-template.js
@@ -56,8 +56,8 @@ The Bitunnel Team.
 exports.textTemplate = errorArray => `
 	Hello,\n
 	Kindly review the following errors thrown within the period under review\n\n
-	Time\t Exception Type\t Ref\t Message\t Start Point\t Failure Point\t Origin User \n
-	${errorArray.map(err => `${err.time}\t ${err.exceptionType}\t ${err.objectReference || 'Nil'}\t ${err.message}\t ${err.startPoint}\t ${err.failurePoint} \t ${err.originUser || 'Nil'}\n
+	Time\t Exception Type\t Ref\t Message \n
+	${errorArray.map(err => `${err.time}\t ${err.source}\t ${err.body || 'Nil'}\t ${err.errorMessage}\n
 	`)}	
 	The Bitunnel Team.
 `;
